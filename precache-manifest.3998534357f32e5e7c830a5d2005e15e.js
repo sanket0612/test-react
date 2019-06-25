9fc8ca5382d536cf74f97d8386d9370612a65f22
sankubunny <sanket.barapatre.06@gmail.com>
2019-06-25 18:14:43 +0530

    Updates

---
+++ precache-manifest.3998534357f32e5e7c830a5d2005e15e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31fedb7acf65d7e5eb8de259a2ac8a3",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "7b99c4f23a500a9e758b",
+    "url": "/test-react/static/css/main.cb74de8b.chunk.css"
+  },
+  {
+    "revision": "6afb4d93dc025bca6d86",
+    "url": "/test-react/static/js/2.74286c36.chunk.js"
+  },
+  {
+    "revision": "7b99c4f23a500a9e758b",
+    "url": "/test-react/static/js/main.f4e45d82.chunk.js"
+  },
+  {
+    "revision": "31d3ffe18f2cb45b7bd6",
+    "url": "/test-react/static/js/runtime~main.bea1702e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
